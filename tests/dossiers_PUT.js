019693018b2a3c8bbaf0fdfaf9a0e672e2ec6ab7
ubuntu <desk@ubuntu.(none)>
2014-06-23 17:54:35 -0300

    les type sont bon

--- tests/dossiers_PUT.js
+++ tests/dossiers_PUT.js
@@ -2,20 +2,18 @@ var requestify = require('requestify');
 var async = require('async');
 
 var unDossier = {
-    "nom" : "OUI",
-    "prenom" : "test_prenom",
-    "codePermanent" : "BOUM15078700",
-    "sexe" : "2",
-    "dateNaissance" : "1987-07-15",
-    "inscriptions" : [
-        {
-            "sigle": "INF4375",
-            "group": "10",
-            "session" : "20142",
-            "noteFinale" : "95"
-        }
-    ],
-    "coursReussis" : [
+    "nom": "OUI",
+    "prenom": "test_prenom",
+    "codePermanent": "BOUM15078700",
+    "sexe": "2",
+    "dateNaissance": "1987-07-15",
+    "inscriptions": [{
+        "sigle": "INF4375",
+        "group": "10",
+        "session": "20142",
+        "noteFinale": "95"
+    }],
+    "coursReussis": [
         "INF4375"
     ]
 }
@@ -24,25 +22,25 @@ var cpDossierAModifier = 'BOUM15078700';
 
 async.series([
 
-    function insererUnDossier(callback) {
-        console.log("Insertion d'un éditeur dans la base de données.");
+        function insererUnDossier(callback) {
+            console.log("Insertion d'un éditeur dans la base de données.");
 
-        requestify.request('http://localhost:3000/dossiers', {
-            method: 'POST',
-            body: unDossier,
-            dataType: 'json'
+            requestify.request('http://localhost:3000/dossiers', {
+                method: 'POST',
+                body: unDossier,
+                dataType: 'json'
 
-        }).then(function(response) {
+            }).then(function (response) {
 
-            console.log("Réponse serveur - code : " + response.getCode());
-            console.log("Réponse serveur - body: " + response.body);
+                console.log("Réponse serveur - code : " + response.getCode());
+                console.log("Réponse serveur - body: " + response.body);
 
-            callback(null, 'insererUnDossier');
-        });
+                callback(null, 'insererUnDossier');
+            });
 
-    },
+        },
 
- /*   function getDossiers(callback) {
+        /*   function getDossiers(callback) {
 
         console.log("Consultation de tous les éditeurs dans la base de donnéés.");
 
@@ -55,84 +53,91 @@ async.series([
         });
     },*/
 
-    function modifierUnDossier(callback) {
-        console.log("\n\nModification de l'éditeur (1).");
-        console.log("cp de l'éditeur à modifier: " + cpDossierAModifier);
+        function modifierUnDossier(callback) {
+            console.log("\n\nModification de l'éditeur (1).");
+            console.log("cp de l'éditeur à modifier: " + cpDossierAModifier);
 
-        var modifDossiers1 = {
-            "nom": "ok",
+            var modifDossiers1 = {
+                "nom": "ok",
 
-            "inscriptions.0.sigle": "INF4000"
+                "inscriptions": [{
+                    "sigle": "INF4370",
+                    "group": "10",
+                    "session": "20142",
+                    "noteFinale": "95"
+                }]
 
 
-        };
-        console.log("\nModifications à apporter: " + JSON.stringify(modifDossiers1, null, 4));
+            };
+            console.log("\nModifications à apporter: " + JSON.stringify(modifDossiers1, null, 4));
 
-        requestify.request('http://localhost:3000/dossiers/' + cpDossierAModifier,
-            {method: 'PUT',
-             body: modifDossiers1,
-             dataType: 'json'}
+            requestify.request('http://localhost:3000/dossiers/' + cpDossierAModifier, {
+                    method: 'PUT',
+                    body: modifDossiers1,
+                    dataType: 'json'
+                }
 
-        ).then(function(response) {
+            ).then(function (response) {
 
-            console.log("Réponse serveur - code : " + response.getCode());
-            console.log("Réponse serveur - body: " + response.body);
+                console.log("Réponse serveur - code : " + response.getCode());
+                console.log("Réponse serveur - body: " + response.body);
 
-            callback(null, 'modifierUnDossier');
-        }).fail(function(response) {
-            console.log("Réponse serveur - code : " + response.getCode());
-            console.log("Réponse serveur - body: " + response.body);
+                callback(null, 'modifierUnDossier');
+            }).fail(function (response) {
+                console.log("Réponse serveur - code : " + response.getCode());
+                console.log("Réponse serveur - body: " + response.body);
 
-            callback(null, 'modifierUnDossier');
-        });
+                callback(null, 'modifierUnDossier');
+            });
 
-    },
+        },
 
-    function modifierUnDossier2(callback) {
-        console.log("\n\nModification du dossier (2).");
-        console.log("_id de l'éditeur à modifier: " + cpDossierAModifier);
+        function modifierUnDossier2(callback) {
+            console.log("\n\nModification du dossier (2).");
+            console.log("_id de l'éditeur à modifier: " + cpDossierAModifier);
 
-        var modifDossiers2 = {
-            "GUI": "no",
-            "website": "www.vim.org"
-        };
+            var modifDossiers2 = {
+                "GUI": "no",
+                "website": "www.vim.org"
+            };
 
-        console.log("\nModifications à apporter: " + JSON.stringify(modifDossiers2, null, 4));
+            console.log("\nModifications à apporter: " + JSON.stringify(modifDossiers2, null, 4));
 
-        requestify.request('http://localhost:3000/dossiers/' + cpDossierAModifier,
-            {method: 'PUT',
-             body: modifDossiers2,
-             dataType: 'json'}
+            requestify.request('http://localhost:3000/dossiers/' + cpDossierAModifier, {
+                    method: 'PUT',
+                    body: modifDossiers2,
+                    dataType: 'json'
+                }
 
-        ).then(function(response) {
+            ).then(function (response) {
 
-            console.log("Réponse serveur - code : " + response.getCode());
-            console.log("Réponse serveur - body: " + response.body);
+                console.log("Réponse serveur - code : " + response.getCode());
+                console.log("Réponse serveur - body: " + response.body);
 
-            callback(null, 'modifierUnDossier2');
+                callback(null, 'modifierUnDossier2');
 
-        }).fail(function(response) {
-            console.log("Réponse serveur - code : " + response.getCode());
-            console.log("Réponse serveur - body: " + response.body);
+            }).fail(function (response) {
+                console.log("Réponse serveur - code : " + response.getCode());
+                console.log("Réponse serveur - body: " + response.body);
 
-            callback(null, 'modifierUnDossier2');
-        });
+                callback(null, 'modifierUnDossier2');
+            });
 
-    },
+        },
 
-    function getDossiersApresSupp(callback) {
+        function getDossiersApresSupp(callback) {
 
-        console.log("Consultation de tous les éditeurs dans la base de donnéés après modification.");
+            console.log("Consultation de tous les éditeurs dans la base de donnéés après modification.");
 
-        requestify.get('http://localhost:3000/dossiers/' + cpDossierAModifier ).then(function(response) {
+            requestify.get('http://localhost:3000/dossiers/' + cpDossierAModifier).then(function (response) {
 
-            console.log("Résultat retourné:" + JSON.stringify(response.getBody(), null, 4));
-            console.log('------------------------------------------------------------------------')
+                console.log("Résultat retourné:" + JSON.stringify(response.getBody(), null, 4));
+                console.log('------------------------------------------------------------------------')
 
-            callback(null, 'getDossiersApresSupp');
-        });
-    }
-],
-    function(err, results) {
+                callback(null, 'getDossiersApresSupp');
+            });
+        }
+    ],
+    function (err, results) {
         console.log("\nFonctions exécutées:\n" + results + "\n");
     });
